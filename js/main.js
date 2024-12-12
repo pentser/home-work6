635627a62f56351bdcd180bb8749de546be4f2a8
eli p <eli.pentser@gmail.com>
2024-12-12 09:32:36 +0200

    update result to input vs alert

--- js/main.js
+++ js/main.js
@@ -1,107 +1,85 @@
-
 let $input = document.querySelector(".input input");
 
-let $row1 =document.querySelector(".row1");
-let $row2 =document.querySelector(".row2");
-let $row3 =document.querySelector(".row3");
-let $row4 =document.querySelector(".row4");
+let $row1 = document.querySelector(".row1");
+let $row2 = document.querySelector(".row2");
+let $row3 = document.querySelector(".row3");
+let $row4 = document.querySelector(".row4");
 
-let operator="";
-let operandX=0;
-let operandY=0; 
+let operator = "";
+let operandX = 0;
+let operandY = 0;
 
-$row1.addEventListener("click",(e)=> {
-    console.log(e.target.id);
-    $input.value+=e.target.id;
-    if(e.target.id=="+") {
-        operator=e.target.id;
-    }
+$row1.addEventListener("click", (e) => {
+  console.log(e.target.id);
+  $input.value += e.target.id;
+  if (e.target.id == "+") {
+    operator = e.target.id;
+  }
+});
+$row2.addEventListener("click", (e) => {
+  console.log(e.target.id);
+  $input.value += e.target.id;
+  if (e.target.id == "-") {
+    operator = e.target.id;
+  }
+});
+$row3.addEventListener("click", (e) => {
+  console.log(e.target.id);
+  $input.value += e.target.id;
+  if (e.target.id == "*") {
+    operator = e.target.id;
+  }
+});
+$row4.addEventListener("click", (e) => {
+  console.log(e.target.id);
 
-})
-$row2.addEventListener("click",(e)=> {
-    console.log(e.target.id);
-    $input.value+=e.target.id;
-    if(e.target.id=="-") {
-        operator=e.target.id;
-    }
-
-})
-$row3.addEventListener("click",(e)=> {
-    console.log(e.target.id);
-    $input.value+=e.target.id;
-    if(e.target.id=="*") {
-        operator=e.target.id;
-    }
-
-})
-$row4.addEventListener("click",(e)=> {
-    console.log(e.target.id);
-    // if(e.target.id=="/") {
-    //     operator="/";
-    // }
-     if(e.target.id=="=") {
-        calculate($input.value);
-    }
-    else if(e.target.id=="C")
+  if (e.target.id == "0") {
+    $input.value += e.target.id;
+  } else if (e.target.id == "=") {
+    calculate($input.value);
+  } else if (e.target.id == "C")
     //? delete last character;
-    $input.value=$input.value.slice(0,-1);
-    else {
-        //? the / operator
-        operator="/";
-        $input.value+=e.target.id;
-    }
-
-
-})
-
+    $input.value = "";
+  else {
+    //? the / operator
+    operator = "/";
+    $input.value += e.target.id;
+  }
+});
 
 function calculate(expretion) {
-    let operands_ar=expretion.split(operator);
-    let operandX=Number(operands_ar[0]); 
-    let operandY=Number(operands_ar[1]);
-    let result;
-    switch(operator) {
-        case "+":
-            result=operandX + operandY;
-            break;
-        case "-":
-         result=operandX - operandY;
-         break;
+  let operands_ar = expretion.split(operator);
+  let operandX = Number(operands_ar[0]);
+  let operandY = Number(operands_ar[1]);
+  let result;
+  switch (operator) {
+    case "+":
+      result = operandX + operandY;
+      break;
+    case "-":
+      result = operandX - operandY;
+      break;
 
-         case "*":
-            result=operandX * operandY;
-            break;
+    case "*":
+      result = operandX * operandY;
+      break;
 
-        case "/":
-            if(operandY==0)     {
-                alert("Cant divide by zero!!!");
-                operandX=0;
-                operandY=0;
-                operator=""
-                $input.value="";
-                return;
-            }
-            else 
-            {
-                result=operandX / operandY;
-            }
-        break;
-    }
+    case "/":
+      if (operandY == 0 || operandY == NaN) {
+        alert("divded by Zero are Forbiten");
+        operandX = 0;
+        operandY = 0;
+        operator = "";
+        $input.value = "";
+        return;
+      } else {
+        result = operandX / operandY;
+      }
+      break;
+  }
 
-   
-    alert(result);
-    operandX=0;
-    operandY=0;
-    operator=""
-    $input.value="";
-
-
-
+  $input.value = result;
+  operandX = 0;
+  operandY = 0;
+  operator = "";
 }
-
-
-
-
-
-
-
